c02e25c9af056d13ab68b6e19ea663a1b3c6cea5
lucianohanzo <68870643+lucianohanzo@users.noreply.github.com>
2024-08-16 19:37:48 -0300

    inicial_bash.sh

--- inicial_bash.sh
+++ inicial_bash.sh
@@ -16,33 +16,32 @@ fi
 
 
 echo "Atualizando o sistema..." ; sleep 4
-apt update 2&> && apt list --upgradable 2&> && apt upgrade -y 2&>
-apt autoremove 2&> && apt autoclean 2&>
+apt update && apt list --upgradable && apt upgrade -y && apt autoremove -y && apt autoclean -y
 
 
 # Complementos do Gedit
 clear ; echo "Instalando o Gedit completo..." ; sleep 4
 apt install -y \
-gedit gedit-plugins gedit-plugin-text-size 2&>
+gedit gedit-plugins gedit-plugin-text-size 2&> /dev/null
 
 # Instalação do Wine.
 clear ; echo "Instalando o Wine completo..." ; sleep 4
-apt install -y wine q4wine 2&>
+apt install -y wine q4wine 2&> /dev/null
 
 # Instalação de Navegadores.
 clear ; echo "Instalando o navegador web básico..." ; sleep 4
-apt install -y falkon 2&>
+apt install -y falkon 2&> /dev/null
 
 # Programas utilitários
 clear ; echo "Instalando o utilitários..." ; sleep 4
 apt install -y \
 vlc qbittorrent vim gparted thunderbird nautilus \
-nemo gnome-font-viewer gdebi gnome-tweaks evince 2&>
+nemo gnome-font-viewer gdebi gnome-tweaks evince 2&> /dev/null
 
 # programas de Edição.
 clear ; echo "Instalando programas de imagem e som..." ; sleep 4
 apt install -y \
-gimp inkscape audacity shotcut obs-studio 2&>
+gimp inkscape audacity shotcut obs-studio 2&> /dev/null
 
 # Colocando idioma em português dos aplicativos.
 clear ; echo "Instalando idioma em português dos aplicativos..."
@@ -50,7 +49,7 @@ echo "Libre Office, ThunderBird e Firefox." ; sleep 4
 apt install -y \
 libreoffice-l10n-pt-br \
 thunderbird-l10n-pt-br \
-firefox-esr-l10n-pt-br 2&>
+firefox-esr-l10n-pt-br 2&> /dev/null
 
 # programas de Jogos
 clear ; echo "Instalando programas de Jogos." ; sleep 4
